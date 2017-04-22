d17256c4bc1cfc34838289c3caa6c3d88183b673
Tommy Poon <tommypoon36@gmail.com>
2017-04-22 19:05:10 -0400

    added buttons

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Tommy Poon on 2017-04-21.
+//  Copyright © 2017 Tommy Poon. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
